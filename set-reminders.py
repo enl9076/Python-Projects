0cb92373177d98ea7041f91def1df423b07f9c48
Emily <enl9076@gmail.com>
2021-11-06 16:43:26 -0400

    Update set-reminders.py

--- set-reminders.py
+++ set-reminders.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from plyer import notification
-import sys
+import PySimpleGUI as gui
 
 datetime_default = datetime.now()
 
@@ -8,15 +8,5 @@ date = ''
 time = ''
 reminder_text = ''
 
-if sys.argv > 1:
-    date = sys.argv[1]
-    time = sys.argv[2]
-    reminder_text = sys.argv[3]
-    print(date, time, reminder_text)
-
-else:
-    date = input('Enter date (YYYY-MM-DD): ')
-    time = input('Enter time (HH:MM): ')
-    reminder_text = input('Enter reminder text: ')
 
 notification.notify(title = "REMEMBER TO", message = f"{reminder_text}", timeout = 10)
